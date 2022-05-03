1afdbab7700a84559ddada312fd05349d57dee28
n00bcyborg <bruceshbmx@gmail.com>
2022-05-03 21:16:43 +1000

    Initial commit

---
+++ Day 2/day_2_project__tip_calculator.py
@@ -0,0 +1,4 @@
+print("Welcome to the tip calculator")
+total = float(input("What was the total bill?\n: $"))
+percentage = int(input("What percantage tip would you like to give? 10, 12, or 15? \n: "))
+people = int(input("How many people to split the bill?\n: "))
---
+++ Day 2/exercise_3_life_in_weeks.py
@@ -0,0 +1,13 @@
+# 🚨 Don't change the code below 👇
+age = input("What is your current age?")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+
+currentage=int(age)
+years_remaining = 90 - currentage
+days_remaining = years_remaining * 365
+weeks_remaining = years_remaining * 52
+months_remaining = years_remaining * 12
+
+print(f"You have {days_remaining} days, {weeks_remaining} weeks, and {months_remaining} months left.")
